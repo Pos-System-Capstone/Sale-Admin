c81977851f008f3ba9017cd0b7f483d69faac2d9
HungHCC <buisihung321@gmail.com>
2021-07-30 16:59:01 +0700

    Add Param serialize

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,8 +2,32 @@ import axios from 'axios';
 
 // ----------------------------------------------------------------------
 
+const parseParams = (params: any) => {
+  const keys = Object.keys(params);
+  let options = '';
+
+  keys.forEach((key) => {
+    const isParamTypeObject = typeof params[key] === 'object';
+    const isParamTypeArray =
+      isParamTypeObject && Array.isArray(params[key]) && params[key].length >= 0;
+
+    if (!isParamTypeObject) {
+      options += `${key}=${params[key]}&`;
+    }
+
+    if (isParamTypeObject && isParamTypeArray) {
+      params[key].forEach((element: any) => {
+        options += `${key}=${element}&`;
+      });
+    }
+  });
+
+  return options ? options.slice(0, -1) : options;
+};
+
 const axiosInstance = axios.create({
-  baseURL: 'https://api-sale.reso.vn/api/v1'
+  baseURL: 'https://api-sale.reso.vn/api/v1',
+  paramsSerializer: parseParams
 });
 
 axiosInstance.interceptors.request.use((options) => {
