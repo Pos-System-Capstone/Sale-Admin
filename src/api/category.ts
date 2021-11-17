166b30bb7e19dcc9ed07ee5d14a9eeedefea7df3
HungHcc <buisihung321@gmail.com>
2021-11-17 10:28:13 +0700

    feat: add modifeir api for category api

--- src/api/category.ts
+++ src/api/category.ts
@@ -1,14 +1,31 @@
 import { TCategory } from 'types/category';
+import { TModifier } from 'types/Modifier';
 import request from 'utils/axios';
-import { generateAPI, generateAPIWithPaging } from './utils';
+import { generateAPIWithPaging } from './utils';
 
 const getExtraCategoriesFromCateId = (catId: number, params?: any) => {
   return request.get(`/categories/${catId}/extras`, { params });
 };
 
+const getModifiersOfCategory = (catId: number) =>
+  request.get<TModifier[]>(`/categories/${catId}/modifiers`);
+
+const addModifiersOfCategory = (catId: number, values: Omit<TModifier, 'id'>) =>
+  request.post<number>(`/categories/${catId}/modifiers`, values);
+
+const updateModifiersOfCategory = (catId: number, modifierId: number, values: TModifier) =>
+  request.put<number>(`/categories/${catId}/modifiers/${modifierId}`, values);
+
+const deleteModifiersOfCategory = (catId: number, modifierId: number) =>
+  request.delete<number>(`/categories/${catId}/modifiers/${modifierId}`);
+
 const categoryApi = {
   ...generateAPIWithPaging<TCategory>('categories'),
-  getExtraCategoriesFromCateId
+  getExtraCategoriesFromCateId,
+  getModifiersOfCategory,
+  addModifiersOfCategory,
+  updateModifiersOfCategory,
+  deleteModifiersOfCategory
 };
 
 export default categoryApi;
