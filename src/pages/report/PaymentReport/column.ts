b3c9a88834dabca18e85c0785496ac4a6ef51598
HungHcc <buisihung321@gmail.com>
2022-08-05 17:45:02 +0700

    feat(payment-report): payment report table for store role

--- src/pages/report/PaymentReport/column.ts
+++ src/pages/report/PaymentReport/column.ts
@@ -1,4 +1,4 @@
-import { TPaymentReportBase } from 'types/report/payment';
+import { TPaymentReportBase, TPaymentReportStore } from 'types/report/payment';
 import { TTableColumn } from 'types/table';
 
 export const paymentColumns: TTableColumn<TPaymentReportBase>[] = [
@@ -12,12 +12,17 @@ export const paymentColumns: TTableColumn<TPaymentReportBase>[] = [
     title: 'Tiền mặt',
     dataIndex: 'cash',
     hideInSearch: true,
-    // render: (value) => formatCurrency(value)
+    valueType: 'money'
+  },
+  {
+    title: 'Nạp thẻ',
+    dataIndex: 'creditCard',
+    hideInSearch: true,
     valueType: 'money'
   },
   {
     title: 'Thẻ thành viên',
-    dataIndex: 'creditCard',
+    dataIndex: 'creditCardUse',
     hideInSearch: true,
     valueType: 'money'
   },
@@ -35,19 +40,19 @@ export const paymentColumns: TTableColumn<TPaymentReportBase>[] = [
   },
   {
     title: 'GrabPay',
-    dataIndex: 'grabpay',
+    dataIndex: 'grabPay',
     hideInSearch: true,
     valueType: 'money'
   },
   {
     title: 'GrabFood',
-    dataIndex: 'grabfood',
+    dataIndex: 'grabFood',
     hideInSearch: true,
     valueType: 'money'
   },
   {
     title: 'VnPay',
-    dataIndex: 'vnpay',
+    dataIndex: 'vnPay',
     hideInSearch: true,
     valueType: 'money'
   },
@@ -59,13 +64,88 @@ export const paymentColumns: TTableColumn<TPaymentReportBase>[] = [
   },
   {
     title: 'ShopeePay',
-    dataIndex: 'shopeepay',
+    dataIndex: 'shopeePay',
     hideInSearch: true,
     valueType: 'money'
   },
   {
     title: 'ZaloPay',
-    // dataIndex: '',
+    dataIndex: 'zaloPay',
+    hideInSearch: true,
+    valueType: 'money'
+  }
+];
+
+export const storePaymentColumns: TTableColumn<TPaymentReportStore>[] = [
+  {
+    title: 'Ngày',
+    dataIndex: 'date',
+    // valueType: 'date',
+    hideInSearch: true,
+    valueType: 'money'
+  },
+  {
+    title: 'Tiền mặt',
+    dataIndex: 'cash',
+    hideInSearch: true,
+    valueType: 'money'
+  },
+  {
+    title: 'Nạp thẻ',
+    dataIndex: 'creditCard',
+    hideInSearch: true,
+    valueType: 'money'
+  },
+  {
+    title: 'Thẻ thành viên',
+    dataIndex: 'creditCardUse',
+    hideInSearch: true,
+    valueType: 'money'
+  },
+  {
+    title: 'Ngân hàng',
+    dataIndex: 'bank',
+    hideInSearch: true,
+    valueType: 'money'
+  },
+  {
+    title: 'Momo',
+    dataIndex: 'momo',
+    hideInSearch: true
+  },
+  {
+    title: 'GrabPay',
+    dataIndex: 'grabPay',
+    hideInSearch: true,
+    valueType: 'money'
+  },
+  {
+    title: 'GrabFood',
+    dataIndex: 'grabFood',
+    hideInSearch: true,
+    valueType: 'money'
+  },
+  {
+    title: 'VnPay',
+    dataIndex: 'vnPay',
+    hideInSearch: true,
+    valueType: 'money'
+  },
+  {
+    title: 'Baemin',
+    dataIndex: 'baemin',
+    hideInSearch: true,
+    valueType: 'money'
+  },
+  {
+    title: 'ShopeePay',
+    dataIndex: 'shopeePay',
+    hideInSearch: true,
+    valueType: 'money'
+  },
+  {
+    title: 'ZaloPay',
+    dataIndex: 'zaloPay',
     hideInSearch: true,
     valueType: 'money'
   }
